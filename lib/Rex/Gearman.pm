10bf62d08ed15c9eb2d9aae69830241eb3497636
Jan <krimdomu@googlemail.com>
2012-08-18 00:03:28 +0200

    documentation

--- lib/Rex/Gearman.pm
+++ lib/Rex/Gearman.pm
@@ -19,50 +19,69 @@ Distributed Rex is setup of two components. One (or more) Worker and one Client.
 
 =head1 INSTALLATION
 
-If you want to test Distributed Rex you have to follow these steps. But keep in mind, this is work in progress. It may crash your systems and eat all your jelly beans and pretzel sticks. And even worse - drink all your beer. You have been warned!
+To install Distributed Rex you have to follow these steps.
 
 
 =head2 On the Client
 
-First you have to download the development branch of Rex 0.32.
+First you have to add the Rex repository to your package manager. To do this read L<http://rexify.org/get/>.
 
- git clone git://github.com/krimdomu/Rex.git rex
- cd rex
- perl Makefile.PL
- make
- make test (perhaps you need to skip this)
- make install
+Now you can install Distributed Rex via your package manager. On Debian or Ubuntu you can use I<apt>.
 
-After that you have to install the Gearman Perl modules.
+ apt-get install rex-gearman
 
- curl -L cpanmin.us | perl - --sudo -n Gearman::Client
+If you want to install it via CPAN you can do this easily with cpanm.
 
-Now you can install Distributed Rex.
+ cpanm Rex::Gearman
 
- git clone git://github.com/krimdomu/distributed-rex.git
- cd distributed-rex
- perl Makefile.PL
- make
- make install
+or
 
+ curl -L get.rexify.org | perl - --sudo -n Rex::Gearman
 
 =head2 On the server (worker)
 
-First you have to install gearman and start it. 
+First you have to install gearman, configure it to listen not only on the loopback device and start it. 
 
-After that you have to distribute your Rexfile (and all associated files (like templates, the "lib" directory, ssh keys, ...) to your worker machines. Please be carefull to name the directory where the Rexfile lives in exact the same way as on your client machine!
+Than you can install Distributed Rex. On Debian or Ubuntu you can use I<apt>.
 
-Now you have to install the Gearman Perl modules.
+ apt-get install rex-gearman
 
- curl -L cpanmin.us | perl - --sudo -n Gearman::Worker
+If you want to install it via CPAN you can do this easily with cpanm.
 
-And Distributed Rex.
+ cpanm Rex::Gearman
 
- git clone git://github.com/krimdomu/distributed-rex.git
- cd distributed-rex
- perl Makefile.PL
- make
- make install
+or
+
+ curl -L get.rexify.org | perl - --sudo -n Rex::Gearman
+
+
+After that you have to distribute your Rexfile (and all associated files (like templates, the "lib" directory, ssh keys, ...) to your worker machines. We recommend to name the directory where you put the Rexfile the same as on your client machine.
+
+Example directory structure on the client:
+
+ /home/user/projects/frontend
+                        |-- Rexfile
+                        |-- client.conf
+                        +-- lib
+                        |   +-- Rex
+                        |       +-- Webserver
+                        |       +-- Apache.pm
+                        |           +-- templates
+                        |               +-- httpd.conf
+                        +-- worker.conf
+
+Example directory structure on the server/worker machine:
+
+ /opt/rex/frontend
+             |-- Rexfile
+             |-- client.conf
+             +-- lib
+             |   +-- Rex
+             |       +-- Webserver
+             |       +-- Apache.pm
+             |           +-- templates
+             |               +-- httpd.conf
+             +-- worker.conf
 
 Now change into the directory where your I<Rexfile> is located and create a file I<worker.conf>.
 
@@ -81,6 +100,8 @@ Now you are able to start the workerprocess.
  rex-gearman-worker -d
 
 =head1 RUNNING A TASK
+
+Switch back to your workstation / the client.
 
 Change to the directory where your I<Rexfile> lives and create a file called I<client.conf>. In this file you have to configure your gearmand servers.
 
